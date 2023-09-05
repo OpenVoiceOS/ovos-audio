02d5d7413abe8d837c6eb437d3019378a1dd5099
JarbasAi <jarbasai@mailfence.com>
2023-09-06 00:34:42 +0100

    deprecate/audio_utils

--- ovos_audio/service.py
+++ ovos_audio/service.py
@@ -1,5 +1,9 @@
+import os
 import os.path
+import subprocess
 import time
+from copy import deepcopy
+from distutils.spawn import find_executable
 from os.path import exists, expanduser
 from threading import Thread, Lock
 
@@ -15,7 +19,6 @@ from ovos_utils.log import LOG
 from ovos_utils.metrics import Stopwatch
 from ovos_utils.process_utils import ProcessStatus, StatusCallbackMap
 from ovos_utils.signal import check_for_signal
-from ovos_utils.sound import play_audio
 
 from ovos_audio.audio import AudioService
 from ovos_audio.tts import TTSFactory
@@ -40,6 +43,55 @@ def on_error(e='Unknown'):
 
 def on_stopping():
     LOG.info('Audio service is shutting down...')
+
+
+# Create a custom environment to use that can be ducked by a phone role.
+# This is kept separate from the normal os.environ to ensure that the TTS
+# role isn't affected and that any thirdparty software launched through
+# a mycroft process can select if they wish to honor this.
+_ENVIRONMENT = deepcopy(os.environ)
+_ENVIRONMENT['PULSE_PROP'] = 'media.role=music'
+
+
+def _get_pulse_environment(config):
+    """Return environment for pulse audio depeding on ducking config."""
+    tts_config = config.get('tts', {})
+    if tts_config and tts_config.get('pulse_duck'):
+        return _ENVIRONMENT
+    else:
+        return os.environ
+
+
+def _find_player(uri):
+    _, ext = os.path.splitext(uri)
+
+    # scan installed executables that can handle playback
+    sox_play = find_executable("play")
+    # sox should handle almost every format, but fails in some urls
+    if sox_play:
+        return sox_play + f" --type {ext} %1"
+    # determine best available player
+    ogg123_play = find_executable("ogg123")
+    if "ogg" in ext and ogg123_play:
+        return ogg123_play + " -q %1"
+    pw_play = find_executable("pw-play")
+    # pw_play handles both wav and mp3
+    if pw_play:
+        return pw_play + " %1"
+    # wav file
+    if 'wav' in ext:
+        pulse_play = find_executable("paplay")
+        if pulse_play:
+            return pulse_play + " %1"
+        alsa_play = find_executable("aplay")
+        if alsa_play:
+            return alsa_play + " %1"
+    # guess mp3
+    mpg123_play = find_executable("mpg123")
+    if mpg123_play:
+        return mpg123_play + " %1"
+    LOG.error("Can't find player for: %s", uri)
+    return None
 
 
 class PlaybackService(Thread):
@@ -87,6 +139,60 @@ class PlaybackService(Thread):
         except Exception as e:
             LOG.exception(e)
             self.status.set_error(e)
+
+    @staticmethod
+    def play_sound_locally(uri, play_cmd=None, environment=None):
+        """Play an audio file. Used by ovos-audio
+
+        This wraps the other play_* functions, choosing the correct one based on
+        the file extension. The function will return directly and play the file
+        in the background.
+
+        Args:
+            uri:    uri to play
+            environment (dict): optional environment for the subprocess call
+
+        Returns: subprocess.Popen object. None if the format is not supported or
+                 an error occurs playing the file.
+        """
+        config = Configuration()
+        environment = environment or _get_pulse_environment(config)
+
+        # NOTE: some urls like youtube streams will cause extension detection to fail
+        # let's handle it explicitly
+        uri = uri.split("?")[0]
+        # Replace file:// uri's with normal paths
+        uri = uri.replace('file://', '')
+
+        _, ext = os.path.splitext(uri)
+
+        if not play_cmd:
+            if "ogg" in ext:
+                play_cmd = config.get("play_ogg_cmdline")
+            elif "wav" in ext:
+                play_cmd = config.get("play_wav_cmdline")
+            elif "mp3" in ext:
+                play_cmd = config.get("play_mp3_cmdline")
+
+        if not play_cmd:
+            play_cmd = _find_player(uri)
+
+        if not play_cmd:
+            LOG.error(f"Failed to play: No playback functionality available")
+            return None
+
+        play_cmd = play_cmd.split(" ")
+
+        for index, cmd in enumerate(play_cmd):
+            if cmd == "%1":
+                play_cmd[index] = uri
+
+        try:
+            return subprocess.Popen(play_cmd, env=environment)
+        except Exception as e:
+            LOG.error(f"Failed to play: {play_cmd}")
+            LOG.exception(e)
+            return None
 
     @staticmethod
     def get_tts_lang_options(lang, blacklist=None):
@@ -414,7 +520,7 @@ class PlaybackService(Thread):
         if not exists(audio_file):
             raise FileNotFoundError(f"{audio_file} does not exist")
 
-        play_audio(audio_file).wait()
+        self.play_sound_locally(audio_file)
 
     def handle_get_languages_tts(self, message):
         """
